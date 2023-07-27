cfab33a8f3f290b67cb874b93a8df69029329c51
mekuanint12 <mekuanint1212@gmail.com>
2023-07-27 14:54:01 +0300

    using dipatch in a useReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,20 +2,26 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-function countReducer(state, action) {
-  return state + action
-}
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  // const [count, setCount] = React.useState(initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(step)
+// const countReducer = (state, action) => ({...state, ...action})
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      break
+    }
+  }
+}
+
+function Counter({initialCount = 0, step = 3}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
