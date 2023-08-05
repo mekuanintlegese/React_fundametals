fb5c8fb8bff160d065a72344c31cdb4b34723092
mekuanint12 <mekuanint1212@gmail.com>
2023-08-05 11:26:01 +0300

    added custom hook for error rendering

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -11,13 +11,21 @@ function CountProvider(props) {
   return <CountContext.Provider value={value} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be defined within the CountProvider')
+  }
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -25,10 +33,6 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
       <CountProvider>
         <CountDisplay />
         <Counter />
